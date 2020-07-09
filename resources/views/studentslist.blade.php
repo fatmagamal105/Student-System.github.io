a6956b9e4e63b50a594114b116e9761662e778e0
fatmagamal105 <66336409+fatmagamal105@users.noreply.github.com>
2020-07-09 16:57:07 +0300

    first commit

---
+++ resources/views/studentslist.blade.php
@@ -0,0 +1,38 @@
+	<div class="card mb-3">
+  <img src="http://inventain.com/wp-content/uploads/2020/01/Children-And-Education-Are-Important-768x403.png" 
+  class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">List of Students</h5>
+    <p class="card-text">You Can find here all the information about students in the system</p>
+  </div>
+  <table class="table">
+	  <thead class="thead-light">
+	    <tr>
+ 	        <th scope="col">CNE</th>
+	        <th scope="col">First Name</th>
+	        <th scope="col">Second Name</th>
+	        <th scope="col">Age</th>
+	        <th scope="col">Speciality</th>
+	       	<th scope="col">Operations</th>
+	    </tr>
+	  </thead>
+	  <tbody>
+	  	@foreach($students as $student)
+	  	<tr>
+	      <td>{{ $student->cne }}</td>
+	      <td>{{ $student->firstName }}</td>
+	      <td>{{ $student->secondName }}</td>
+	      <td>{{ $student->age }}</td>
+	      <td>{{ $student->speciality }}</td>
+	      <td>
+	      	<a href="{{ url('/edit/' . $student->id) }}" class="btn btn-sm btn-warning">Edit</a>
+	      </td>
+	    </tr>
+	    @endforeach
+	  </tbody>
+	</table>
+</div>
+
+
+
+	
